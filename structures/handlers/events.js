5d690051eec514580811af95dce28f84b9e53564
Julexar <viktor.thorin@gmail.com>
2023-09-22 11:37:49 +0000

    Initial Commit

---
+++ structures/handlers/events.js
@@ -0,0 +1,32 @@
+const { glob } = require('glob');
+const { promisify } = require('util');
+const promiseGlob = promisify(glob);
+const Ascii = require('ascii-table');
+/**
+ * 
+ * @param {import('../index')} client 
+ */
+module.exports = async (client) => {
+    const eventsTable = new Ascii('Events').setHeading('Name', 'Status', 'Reason');
+    (await promiseGlob(`${process.cwd().replace(/\\/g, '/')}/events/*/*.js`)).map(async (file) => {
+        const event = require(file);
+        const P = file.split('/');
+        let name;
+
+        if(!event.name || !event.run) 
+        return eventsTable.addRow(`${event.name || `${P[P.length - 1]}/${P[P.length - 2]}`}`, 'Failed', 'Missing Name/Run');
+
+        if(event.nick) 
+        name = `${event.name} (${event.nick})`
+        else
+        name = event.name;
+
+        if(event.once) 
+        client.once(event.name, (...args) => event.run(...args, client))
+        else
+        client.on(event.name, (...args) => event.run(...args, client));
+
+        eventsTable.addRow(name, 'Success');
+    });
+    console.log(eventsTable.toString());
+};
---
+++ structures/lib/DiscordClient.js
@@ -0,0 +1,44 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const db = require("../../database/db");
+require("dotenv").config()
+class DiscordClient extends Client {
+    constructor() {
+        super({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.GuildMembers
+            ]
+        });
+
+        this.class_icons = {
+            'Warrior': 'https://file.mir4global.com/xdraco/img/common/ranking/ico_klass_warrior.webp',
+            'Sorcerer': 'https://file.mir4global.com/xdraco/img/common/ranking/ico_klass_sorcerer.webp',
+            'Taoist': 'https://file.mir4global.com/xdraco/img/common/ranking/ico_klass_taoist.webp',
+            'Lancer': 'https://file.mir4global.com/xdraco/img/common/ranking/ico_klass_lancer.webp',
+            'Arbalist': 'https://file.mir4global.com/xdraco/img/common/ranking/ico_klass_arbalist.webp',
+            'Darkist': 'https://file.mir4global.com/xdraco/img/common/ranking/ico_klass_darkist.webp'
+        };
+
+        this.class_banners = {
+            'Warrior': 'https://file.mir4global.com/xdraco/img/common/nft-detail/nft-detail-warrior5.webp',
+            'Sorcerer': 'https://file.mir4global.com/xdraco/img/common/nft-detail/nft-detail-sorcerer5.webp',
+            'Taoist': 'https://file.mir4global.com/xdraco/img/common/nft-detail/nft-detail-taoist5.webp',
+            'Lancer': 'https://file.mir4global.com/xdraco/img/common/nft-detail/nft-detail-lancer5.webp',
+            'Arbalist': 'https://file.mir4global.com/xdraco/img/common/nft-detail/nft-detail-arbalist5.webp',
+            'Darkist': 'https://file.mir4global.com/xdraco/img/common/nft-detail/nft-detail-darkist5.webp'
+        };
+
+        this.db = db;
+    };
+
+    async start() {
+        ["events","slashCommands"].forEach(handler => {
+            require(`../handlers/${handler}`)(this);
+        });
+
+        this.login(process.env.TOKEN);
+    }
+};
+module.exports = DiscordClient;
